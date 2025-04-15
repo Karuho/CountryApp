af0d6d69303f21168a9cbeeb7f6f439975e5b1ba
Carlos Torres <carlos.to.fa87@gmail.com>
2025-04-15 02:11:04 -0400

    Update changes

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { CountryService } from '../../services/country.service';
+import { NotFoundComponent } from '../../../shared/components/not-found/not-found.component';
 
 @Component({
   selector: 'country-page',
-  imports: [],
+  imports: [NotFoundComponent],
   templateUrl: './country-page.component.html',
 })
 export class CountryPageComponent {
